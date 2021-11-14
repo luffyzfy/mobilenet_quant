aac2d88ff0755c23102a01dd5699b88430364463
luffyzfy <43133753+luffyzfy@users.noreply.github.com>
2021-11-14 23:20:21 +0800

    Add files via upload

---
+++ 神经网络量化v3_1.py
@@ -0,0 +1,570 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import torch
+import math
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import torchvision.transforms as transforms
+import os
+import time
+import sys
+import torch.quantization
+
+# # Setup warnings
+import warnings
+warnings.filterwarnings(
+    action='ignore',
+    category=DeprecationWarning,
+    module=r'.*'
+)
+warnings.filterwarnings(
+    action='default',
+    module=r'torch.quantization'
+)
+
+# Specify random seed for repeatable results
+torch.manual_seed(191009)
+
+
+# In[2]:
+
+
+from torch.quantization import QuantStub, DeQuantStub
+
+"""
+Creates a MobileNetV3 Model as defined in:
+Andrew Howard, Mark Sandler, Grace Chu, Liang-Chieh Chen, Bo Chen, Mingxing Tan, Weijun Wang, Yukun Zhu, Ruoming Pang, Vijay Vasudevan, Quoc V. Le, Hartwig Adam. (2019).
+Searching for MobileNetV3
+arXiv preprint arXiv:1905.02244.
+"""
+
+
+
+def _make_divisible(v, divisor, min_value=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    :param v:
+    :param divisor:
+    :param min_value:
+    :return:
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=4):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, _make_divisible(channel // reduction, 8)),
+                nn.ReLU(inplace=True),
+                nn.Linear(_make_divisible(channel // reduction, 8), channel),
+                h_sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+def conv_3x3_bn(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        h_swish()
+    )
+
+
+def conv_1x1_bn(inp, oup):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        h_swish()
+    )
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, hidden_dim, oup, kernel_size, stride, use_se, use_hs):
+        super(InvertedResidual, self).__init__()
+        assert stride in [1, 2]
+
+        self.identity = stride == 1 and inp == oup
+
+        if inp == hidden_dim:
+            self.conv = nn.Sequential(
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                h_swish() if use_hs else nn.ReLU(inplace=True),
+                # Squeeze-and-Excite
+                SELayer(hidden_dim) if use_se else nn.Identity(),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        else:
+            self.conv = nn.Sequential(
+                # pw
+                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                h_swish() if use_hs else nn.ReLU(inplace=True),
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                # Squeeze-and-Excite
+                SELayer(hidden_dim) if use_se else nn.Identity(),
+                h_swish() if use_hs else nn.ReLU(inplace=True),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+
+    def forward(self, x):
+        if self.identity:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class MobileNetV3(nn.Module):
+    def __init__(self, cfgs, mode, num_classes=1000, width_mult=1.):
+        super(MobileNetV3, self).__init__()
+        # setting of inverted residual blocks
+        self.cfgs = cfgs
+        assert mode in ['large', 'small']
+
+        # building first layer
+        input_channel = _make_divisible(16 * width_mult, 8)
+        layers = [conv_3x3_bn(3, input_channel, 2)]
+        # building inverted residual blocks
+        block = InvertedResidual
+        for k, t, c, use_se, use_hs, s in self.cfgs:
+            output_channel = _make_divisible(c * width_mult, 8)
+            exp_size = _make_divisible(input_channel * t, 8)
+            layers.append(block(input_channel, exp_size, output_channel, k, s, use_se, use_hs))
+            input_channel = output_channel
+        self.features = nn.Sequential(*layers)
+        # building last several layers
+        self.conv = conv_1x1_bn(input_channel, exp_size)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        output_channel = {'large': 1280, 'small': 1024}
+        output_channel = _make_divisible(output_channel[mode] * width_mult, 8) if width_mult > 1.0 else output_channel[mode]
+        self.classifier = nn.Sequential(
+            nn.Linear(exp_size, output_channel),
+            h_swish(),
+            nn.Dropout(0.2),
+            nn.Linear(output_channel, num_classes),
+        )
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.conv(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+def mobilenetv3_large(**kwargs):
+    """
+    Constructs a MobileNetV3-Large model
+    """
+    cfgs = [
+        # k, t, c, SE, HS, s
+        [3,   1,  16, 0, 0, 1],
+        [3,   4,  24, 0, 0, 2],
+        [3,   3,  24, 0, 0, 1],
+        [5,   3,  40, 1, 0, 2],
+        [5,   3,  40, 1, 0, 1],
+        [5,   3,  40, 1, 0, 1],
+        [3,   6,  80, 0, 1, 2],
+        [3, 2.5,  80, 0, 1, 1],
+        [3, 2.3,  80, 0, 1, 1],
+        [3, 2.3,  80, 0, 1, 1],
+        [3,   6, 112, 1, 1, 1],
+        [3,   6, 112, 1, 1, 1],
+        [5,   6, 160, 1, 1, 2],
+        [5,   6, 160, 1, 1, 1],
+        [5,   6, 160, 1, 1, 1]
+    ]
+    return MobileNetV3(cfgs, mode='large', **kwargs)
+
+
+def mobilenetv3_small(**kwargs):
+    """
+    Constructs a MobileNetV3-Small model
+    """
+    cfgs = [
+        # k, t, c, SE, HS, s
+        [3,    1,  16, 1, 0, 2],
+        [3,  4.5,  24, 0, 0, 2],
+        [3, 3.67,  24, 0, 0, 1],
+        [5,    4,  40, 1, 1, 2],
+        [5,    6,  40, 1, 1, 1],
+        [5,    6,  40, 1, 1, 1],
+        [5,    3,  48, 1, 1, 1],
+        [5,    3,  48, 1, 1, 1],
+        [5,    6,  96, 1, 1, 2],
+        [5,    6,  96, 1, 1, 1],
+        [5,    6,  96, 1, 1, 1],
+    ]
+
+    return MobileNetV3(cfgs, mode='small', **kwargs)
+
+
+
+
+# In[3]:
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def evaluate(model, criterion, data_loader, neval_batches):
+    model.eval()
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    cnt = 0
+    with torch.no_grad():
+        for image, target in data_loader:
+            assert image != None
+            image = torch.autograd.Variable(image)
+            output = model(image)
+            loss = criterion(output, target)
+            cnt += 1
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            print('.', end = '')
+            top1.update(acc1[0], image.size(0))
+            top5.update(acc5[0], image.size(0))
+            if cnt >= neval_batches:
+                 return top1, top5
+
+    return top1, top5
+
+def load_model(model_file):
+    model = mobilenetv3_small()
+    state_dict = torch.load(model_file)
+    model.load_state_dict(state_dict)
+    model.to('cpu')
+    return model
+
+def print_size_of_model(model):
+    torch.save(model.state_dict(), "temp.p")
+    print('Size (MB):', os.path.getsize("temp.p")/1e6)
+    os.remove('temp.p')
+
+
+# In[4]:
+
+
+def prepare_data_loaders(data_path):
+
+    traindir = os.path.join(data_path, 'train')
+    valdir = os.path.join(data_path, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    dataset = torchvision.datasets.ImageFolder(
+        traindir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+
+    dataset_test = torchvision.datasets.ImageFolder(
+        valdir,
+        transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+
+    train_sampler = torch.utils.data.RandomSampler(dataset)
+    test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=train_batch_size,
+        sampler=train_sampler)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=eval_batch_size,
+        sampler=test_sampler)
+
+    return data_loader, data_loader_test
+
+
+# In[5]:
+
+
+data_path = 'imagenet_1k'
+saved_model_dir = 'models/'
+#float_model_file = 'mobilenet_pretrained_float.pth'
+float_model_file = 'mobilenetv3-small-55df8e1f.pth'
+scripted_float_model_file = 'mobilenet_quantization_scripted.pth'
+scripted_quantized_model_file = 'mobilenet_quantization_scripted_quantized.pth'
+
+train_batch_size = 30
+eval_batch_size = 30
+
+data_loader, data_loader_test = prepare_data_loaders(data_path)
+criterion = nn.CrossEntropyLoss()
+float_model = load_model(saved_model_dir + float_model_file).to('cpu')
+
+
+# In[6]:
+
+
+print('\n Inverted Residual Block: Before fusion \n\n', float_model.features[1].conv)
+float_model.eval()
+
+
+# In[7]:
+
+
+num_eval_batches = 10
+
+print("Size of baseline model")
+print_size_of_model(float_model)
+
+top1, top5 = evaluate(float_model, criterion, data_loader_test, neval_batches=num_eval_batches)
+print('Evaluation accuracy on %d images, %2.2f'%(num_eval_batches * eval_batch_size, top1.avg))
+torch.jit.save(torch.jit.script(float_model), saved_model_dir + scripted_float_model_file)
+
+
+# In[8]:
+
+
+num_calibration_batches = 10
+
+myModel = load_model(saved_model_dir + float_model_file).to('cpu')
+myModel.eval()
+
+# Specify quantization configuration
+# Start with simple min/max range estimation and per-tensor quantization of weights
+myModel.qconfig = torch.quantization.get_default_qconfig("fbgemm")
+print(myModel.qconfig)
+torch.quantization.prepare(myModel, inplace=True)
+
+# Calibrate first
+print('Post Training Quantization Prepare: Inserting Observers')
+print('\n Inverted Residual Block:After observer insertion \n\n', myModel.features[1].conv)
+
+# Calibrate with the training set
+evaluate(myModel, criterion, data_loader, neval_batches=num_calibration_batches)
+print('Post Training Quantization: Calibration done')
+
+# Convert to quantized model
+model_int8 = torch.quantization.convert(myModel, inplace=True)
+print('Post Training Quantization: Convert done')
+print('\n Inverted Residual Block: After fusion and quantization, note fused modules: \n\n',myModel.features[1].conv)
+
+print("Size of model after quantization")
+print_size_of_model(myModel)
+# torch.save(model_int8.state_dict(), "./mobilenetv3_quant.pth")
+#
+# state_dict = torch.load("./mobilenetv3_quant.pth")
+# model_fp32 = mobilenetv3_small()
+# model_fp32.qconfig = torch.quantization.get_default_qconfig("fbgemm")
+# model_fp32_prepared = torch.quantization.prepare(myModel, inplace=True)
+# model_int8 = torch.quantization.convert(model_fp32_prepared, inplace=True)
+# model_int8.load_state_dict("./mobilenetv3_quant.pth")
+model = model_int8
+model.eval()
+
+
+top1, top5 = evaluate(myModel, criterion, data_loader_test, neval_batches=num_eval_batches)
+print('Evaluation accuracy on %d images, %2.2f'%(num_eval_batches * eval_batch_size, top1.avg))
+
+# 参考链接
+# https://www.jb51.net/article/177479.htm
+
+# In[24]:
+
+'''
+qm = torch.nn.quantized.Quantize()
+dqm = torch.nn.quantized.DeQuantize()
+
+
+# In[90]:
+
+
+state = per_channel_quantized_model.state_dict()
+
+
+# In[105]:
+
+
+features = per_channel_quantized_model.features._modules
+x = images
+print("original:\n", x[:, :, 0:3, 0:3])
+x = per_channel_quantized_model.quant(x)
+print("quant:\n", x[:, :, 0:3, 0:3].int_repr())
+x_ = x.clone()
+print(x)
+x = features["0"](x)
+print(features["0"].state_dict().keys())
+weight     = features["0"].state_dict()['0.weight']
+bias       = features["0"].state_dict()['0.bias']
+zero_point = features["0"].state_dict()['0.zero_point']
+scale      = features["0"].state_dict()['0.scale']
+x_ = torch.nn.functional.conv2d(x_, weight, bias=bias, stride=1, padding=0, dilation=1, groups=1)
+print(x.shape)
+print(x_.shape)
+print("fea0:\n", x[0][1])
+plt.imshow(x[0][1].int_repr())
+print()
+
+
+# In[107]:
+
+
+endings = {}
+for name in per_channel_quantized_model.state_dict():
+    #print(name, state[name].shape, state[name].dtype)
+    #if name.endswith("weight"):
+    #    print(state[name].int_repr()[0])
+    #    print(state[name].stride())
+    if name.endswith("scale"):
+        print(state[name])
+    words = name.split('.')
+    ending = words[-1]
+    if ending not in endings:
+        endings[ending] = 1
+    else: 
+        endings[ending] += 1
+
+
+# In[ ]:
+
+
+state
+
+
+# In[18]:
+
+
+print(endings.keys())
+
+
+# In[47]:
+
+
+save_dir = saved_model_dir + 'quant/'
+
+var_cnt = 0
+for name in state:
+    words = name.split('.')
+    prefix, ending = '.'.join(words[:len(words)-1]), words[-1]
+    x = state[name]
+    if ending == 'weight':
+        ret = x.int_repr()
+        np.save(save_dir+name+'.npy', ret.numpy())
+    elif ending == 'bias':
+        ret = x.detach()
+        np.save(save_dir+name+'.npy', ret.numpy())
+    else:
+        ret = x.item()
+        np.save(save_dir+name+'.npy', ret)
+
+
+# In[11]:
+
+
+state = myModel.state_dict()
+for name in myModel.state_dict():
+    print(name)
+    print(state[name])
+'''
